c28d9be53a070127745979589a847bf0cbc6ee18
Michal Němec <michaelnemec47@gmail.com>
2024-05-06 16:11:24 +0200

    Add OpenSSL manually

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NFCPassportReader",
-    platforms: [.iOS("15.0")],
+    platforms: [.iOS("15.0"), .macOS(.v11)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", .upToNextMinor(from: "1.1.1900"))
+//        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", .upToNextMinor(from: "1.1.1900")),
 
     ],
     targets: [
@@ -23,7 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "NFCPassportReader",
-            dependencies: ["OpenSSL"]),
+            dependencies: ["ReaderOpenSSL"]
+        ),
+        .binaryTarget(name: "ReaderOpenSSL", path: "./Sources/OpenSSL.xcframework")
+
     ]
 )
-
